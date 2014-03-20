7d4e3bf7a40c377978eaa09eb769922dbc710c5c
Hannah <hannah_wight@hotmail.com>
2014-03-20 18:37:27 +0000

    button now works, but have to delete cookies every session

--- sudoku.rb
+++ sudoku.rb
@@ -13,7 +13,7 @@ end
 
 # this method removes some digits from the solution to create a puzzle
 def puzzle(sudoku)
-  sudoku.map { |element| rand > 0.37 ? ' ' : element }
+  sudoku.map { |element| rand > 0.37 ? 0 : element }
 end
 
 def generate_new_puzzle_if_necessary
@@ -40,17 +40,37 @@ end
 
 post '/' do
   cells = params["cell"]
-  session[:current_solution] = cells.map{|value| value.to_i }.join
+  session[:current_solution] = box_order_to_row_order(cells)
   session[:check_solution] = true
   redirect to("/")
 end
 
 get '/solution' do
   @current_solution = session[:solution]
-  erb :index
+  # erb :index
 end
 
+
+def box_order_to_row_order(cells)
+  boxes = cells.each_slice(9).to_a
+    (0..8).to_a.inject([]) {|memo, i|
+      first_box_index = i / 3 * 3
+      three_boxes = boxes[first_box_index, 3]
+      three_rows_of_three = three_boxes.map do |box| 
+        row_number_in_a_box = i % 3
+        first_cell_in_the_row_index = row_number_in_a_box * 3
+        box[first_cell_in_the_row_index, 3]
+      end
+      memo += three_rows_of_three.flatten
+    }
+  end
+
+
 helpers do
+
+	def clean_up_cell_value(value)
+    value.to_i == 0 ? '' : value
+  end  
 
   def colour_class(solution_to_check, puzzle_value, current_solution_value, solution_value)
     must_be_guessed = puzzle_value == 0
